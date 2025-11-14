e19dac902f25b7171c8948269a2ea9f4ffc68dfb
Joel <joel.stark@stud.fra-uas.de>
2025-11-15 00:02:21 +0100

    Random testing changes
    i dont even know + code to spawn the window in the middle of the screen at 60% size

--- src/main/java/org/group_three/basicGui/MainGui.java
+++ src/main/java/org/group_three/basicGui/MainGui.java
@@ -1,23 +1,19 @@
 package org.group_three.basicGui;
 
 import javafx.application.Application;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-
 import javafx.animation.PauseTransition;
 import javafx.util.Duration;
-
 import javafx.animation.RotateTransition;
-import javafx.application.Application;
 import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.transform.Rotate;
-import javafx.stage.Stage;
-import javafx.util.Duration;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 
 
 
@@ -52,35 +48,10 @@ public class MainGui extends Application {
 	}
 
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage stage) throws Exception {
 
-
-		Label label2 = new Label("Hallo JavaFX 25 mit Maven!");
-		StackPane root2 = new StackPane(label2);
-		Scene scene2 = new Scene(root2, 400, 300);
-
-		root2.setStyle("-fx-background-color: #BFD8EE;"); // Blau
-
-		Label label = new Label("Hallo JavaFX 25 mit Maven!");
-		StackPane root = new StackPane(label);
-		Scene scene = new Scene(root, 400, 300);
-
-		root.setStyle("-fx-background-color: #171D25;"); // Blau
-
-		stage.setTitle(windowTitle);
-		stage.getIcons().add(getAppIcon());
-		stage.setScene(scene);
-		stage.show();
-
-		PauseTransition delay = new PauseTransition(Duration.seconds(5));
-		delay.setOnFinished(event -> {
-			// neue Scene hier setzen
-			stage.setScene(scene2);
-			System.out.println("Switched scenes.");
-		});
-		delay.play();
+		//--------------------------------------
 		
-		/*
 		// GPT EXAMPLE
 		// 3D Objekt
         Box box = new Box(100, 100, 100);
@@ -88,6 +59,13 @@ public class MainGui extends Application {
         material.setDiffuseColor(Color.DODGERBLUE);
         material.setSpecularColor(Color.LIGHTBLUE);
         box.setMaterial(material);
+
+		// 3D Objekt
+        Box box2 = new Box(200, 50, 50);
+        PhongMaterial material2 = new PhongMaterial();
+        material2.setDiffuseColor(Color.DODGERBLUE);
+        material2.setSpecularColor(Color.LIGHTBLUE);
+        box2.setMaterial(material2);
 
         // Licht
         PointLight light = new PointLight(Color.WHITE);
@@ -112,12 +90,8 @@ public class MainGui extends Application {
         subScene.setFill(Color.GRAY);
 
         // Haupt-Root (2D), hier könnte auch UI drüber liegen
-        Group root = new Group(subScene);
-        Scene scene = new Scene(root, 800, 600, true);
-
-        stage.setTitle("JavaFX 3D Beispiel");
-        stage.setScene(scene);
-        stage.show();
+        Group root3d = new Group(subScene);
+        Scene scene3d = new Scene(root3d, 800, 600, true);
 
         // einfache Rotation-Animation
         RotateTransition rt = new RotateTransition(Duration.seconds(3), box);
@@ -125,7 +99,69 @@ public class MainGui extends Application {
         rt.setFromAngle(0);
         rt.setToAngle(360);
         rt.setCycleCount(RotateTransition.INDEFINITE);
-        rt.play();*/
+        rt.play();
+		//----------------------------
+
+
+		/*
+		Label label2 = new Label("Hallo JavaFX 25 mit Maven!");
+		StackPane root2 = new StackPane(label2);
+		Scene scene2 = new Scene(root2, 400, 300);
+
+		root2.setStyle("-fx-background-color: #BFD8EE;"); // Blau
+
+		Label label = new Label("Hallo JavaFX 25 mit Maven!");
+		StackPane root = new StackPane(label);
+		Scene scene = new Scene(root, 400, 300);
+
+		root.setStyle("-fx-background-color: #171D25;"); // Blau*/
+
+		FXMLLoader loader = new FXMLLoader(
+			getClass().getResource("/org/group_three/basicGui/fxml/MainWindow.fxml")
+		);
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+
+
+		// SPawn Window in the middle of the screen at 60% monitor size
+		Rectangle2D screen = Screen.getPrimary().getVisualBounds();
+
+        double w = screen.getWidth() * 0.60;
+        double h = screen.getHeight() * 0.60;
+
+        stage.setWidth(w);
+        stage.setHeight(h);
+
+        stage.setX((screen.getWidth() - w) / 2);
+        stage.setY((screen.getHeight() - h) / 2);
+
+		System.out.println(stage.getX() + ":" + stage.getY());
+		//-------------------------------------------------------------
+		
+
+        // CSS hinzufügen
+        //scene.getStylesheets().add(
+        //        getClass().getResource("style.css").toExternalForm()
+        //);
+
+
+
+		stage.setTitle(windowTitle);
+		stage.getIcons().add(getAppIcon());
+		stage.setScene(scene);
+		stage.show();
+
+		/*PauseTransition delay = new PauseTransition(Duration.seconds(5));
+		delay.setOnFinished(event -> {
+			// neue Scene hier setzen
+			stage.setScene(scene2);
+			System.out.println("Switched scenes.");
+		});
+		delay.play();*/
+		
 	}
 
 	public static void main(String[] args) {
