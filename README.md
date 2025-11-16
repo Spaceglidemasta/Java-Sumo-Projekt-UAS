4735466db3643a60ff9c3a161eec34f1cfac8344
Spaceglidemasta <lucastarwars1@gmail.com>
2025-11-16 14:34:27 +0100

    readme init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Project WiSe 25 / 26, Group 3
+
+By Luca De Simone, Leon Chimentchik, Joel Stark and David Cornelius.
--- src/main/java/org/group_three/Main.java
+++ src/main/java/org/group_three/Main.java
@@ -1,17 +1,17 @@
 package org.group_three;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.eclipse.sumo.libsumo.*;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Simulation.preloadLibraries();
+        Simulation.start(new StringVector(new String[] {"sumo", "-n", "src/main/resources/net.net.xml",
+                                                                "-r", "src/main/resources/net.rou.xml"
+                                                        }));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Program start HERE");
+
     }
 }
