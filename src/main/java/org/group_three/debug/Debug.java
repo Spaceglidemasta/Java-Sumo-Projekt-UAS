f19a61aab6b3db2e3a768e9ad14ded2582293491
Joel <joel.stark@stud.fra-uas.de>
2025-11-17 16:29:04 +0100

    + class name color = cyan & added multi type compatibility

--- src/main/java/org/group_three/debug/Debug.java
+++ src/main/java/org/group_three/debug/Debug.java
@@ -6,17 +6,18 @@ public class Debug {
 
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_CYAN = "\u001B[36m";
     private static final String BOLD = "\033[0;1m";
 
 
 
-    public static void print(String s){
+    public static void print(Object value){
 
         if(do_debug){
             StackTraceElement caller = Thread.currentThread().getStackTrace()[2];
             String className = caller.getClassName().substring(16);
 
-            System.out.println(BOLD + "[" + ANSI_BLUE + "DEBUG" + ANSI_RESET + BOLD + "](" + className + ") " + ANSI_RESET + s );
+            System.out.println(BOLD + "[" + ANSI_BLUE + "DEBUG" + ANSI_RESET + BOLD + "](" + ANSI_CYAN + className + ANSI_RESET + ") " + ANSI_RESET + String.valueOf(value));
         }
 
     }
