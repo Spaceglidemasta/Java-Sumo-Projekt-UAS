b24877fe71414251f6b3c5854a24ab88e9d83682
Joel <joel.stark@stud.fra-uas.de>
2025-11-25 09:54:19 +0100

    + comments in const UI class

--- src/main/java/org/group_three/constants/UI.java
+++ src/main/java/org/group_three/constants/UI.java
@@ -3,9 +3,18 @@ package org.group_three.constants;
 public final class UI {
     private UI() {} //prevents init.
 
+	// The window name which can be seen next to the icon in the window itself
+	// and the name when you hover over the window in the taskbar.
 	public static final String appTitle = "Java-Sumo-Projekt-UAS";
+	// The image which is loaded as the main window icon and as the icon in the taskbar.
 	public static final String appIcon = "/org/group_three/ui/icons/SumoLogoAdjustments3.png";
+	// The main FXML class which is loaded into the main window.
+	// Other UI/FXML parts will be loaded into it.
 	public static final String appFXML = "/org/group_three/ui/fxml/MainWindow.fxml";
+
+
+	// The minimal window width.
 	public static final int appMinWidth = 960;
+	// The minimal window height.
 	public static final int appMinHeight = 540;
 }
