b26518bb7010375e61606678ab0ee5eae95ea644
Joel <joel.stark@stud.fra-uas.de>
2025-11-28 21:52:44 +0100

    removal of SimulationView2D and 3D classes as they had no purpose

--- src/main/java/org/group_three/ui/controllers/BodyController.java
+++ src/main/java/org/group_three/ui/controllers/BodyController.java
@@ -2,8 +2,8 @@ package org.group_three.ui.controllers;
 
 import java.io.IOException;
 
-import org.group_three.ui.idkyet.SimulationView2D;
-import org.group_three.ui.idkyet.SimulationView3D;
+//import org.group_three.ui.idkyet.SimulationView2D;
+//import org.group_three.ui.idkyet.SimulationView3D;
 import org.group_three.debug.Debug;
 
 import javafx.fxml.FXML;
@@ -22,28 +22,28 @@ public class BodyController {
 	@FXML
 	private Pane binder;
 
-	private SimulationView3D sv3d;
-	private SimulationView2D sv2d;
+	//private SimulationView3D sv3d;
+	//private SimulationView2D sv2d;
 
 	@FXML
 	public void initialize() throws IOException {
 		Debug.toConsole("Body loaded.");
 		//Debug.toConsole("SubScene: " + subsceneView);
 
-		sv3d = new SimulationView3D();
-		sv2d = new SimulationView2D();
+		//sv3d = new SimulationView3D();
+		//sv2d = new SimulationView2D();
 
 		subsceneView.widthProperty().bind(binder.widthProperty());
 		subsceneView.heightProperty().bind(binder.heightProperty());
 
 		switch (2) {
 			case 0:
-				subsceneView.setRoot(sv3d.createView());
-				subsceneView.setCamera(new PerspectiveCamera());
+				//subsceneView.setRoot(sv3d.createView());
+				//subsceneView.setCamera(new PerspectiveCamera());
 				break;
 
 			case 1:
-				subsceneView.setRoot(sv2d.createView());
+				//subsceneView.setRoot(sv2d.createView());
 				break;
 
 			case 2:
