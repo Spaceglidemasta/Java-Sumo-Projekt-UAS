ef33f98f27f8a507d8344b3dfd6e1b4639f963bb
masta of the universe <lucastarwars1@gmail.com>
2025-11-30 18:19:33 +0100

    Added StaticTester Class so the main functio isnt cluttered wit testing

---
+++ src/main/java/org/group_three/debug/StaticTester.java
@@ -0,0 +1,67 @@
+package org.group_three.debug;
+
+
+import de.tudresden.sumo.cmd.Route;
+import de.tudresden.sumo.cmd.Trafficlight;
+import de.tudresden.sumo.cmd.Vehicle;
+import de.tudresden.sumo.objects.SumoStringList;
+import org.group_three.api.SimController;
+import org.group_three.model.WVehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tester Class so we don't have the Main function cluttered full of
+ * Random test methods. There is no other use for this.
+ * @author Luca
+ * */
+public class StaticTester {
+
+    public StaticTester() {
+        SimController sim = new SimController("speedtest.sumocfg");
+
+        //Test Scenario
+
+        Debug.print("Trafic lights: " + sim.jobget(Trafficlight.getIDList()).toString());
+
+        Debug.print("Edges:" + sim.getEdgeIDList());
+
+        Debug.print("Vehicles:" + sim.getVehicleIDList());
+
+        Debug.print("Vehicle Count: " + sim.jobget(Vehicle.getIDCount()));
+
+        List<String> edgelist = new ArrayList<>();
+        edgelist.add("-E2");
+        edgelist.add("E1.200");
+
+        SumoStringList edges = new SumoStringList(edgelist);
+
+        sim.jobset(Route.add("r_1", edges));
+
+        Debug.print("Routes:" + sim.getRouteIDList());
+
+        WVehicle v = sim.addVehicle("DEFAULT_CONTAINERTYPE", "r_1", 2, 0, 1, (byte)0);
+
+        Debug.print(v.getPos());
+
+        Debug.print("Junction Positions");
+        for(String junc : sim.getJunctionIDList()){
+            Debug.print(junc + ": " + sim.getJunctionPos(junc));
+        }
+
+        Debug.print("TL phases");
+        for(String tl : sim.getTrafficLightsIDList()){
+            Debug.print(tl + ": " + sim.getTLPhase(tl));
+        }
+
+        Debug.print("Vehicle Positions");
+        for(String veh : sim.getVehicleIDList()){;
+            Debug.print(veh + ": " + sim.jobget(Vehicle.getPosition(veh)));
+        }
+
+        sim.close();
+    }
+
+
+}
