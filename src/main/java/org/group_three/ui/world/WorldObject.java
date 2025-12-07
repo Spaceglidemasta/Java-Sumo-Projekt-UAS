79c18a8158ab6909efc51ac58be286b889cab760
Joel <joel.stark@stud.fra-uas.de>
2025-12-07 17:56:43 +0100

    world object detail class placeholders

--- src/main/java/org/group_three/ui/world/WorldObject.java
+++ src/main/java/org/group_three/ui/world/WorldObject.java
@@ -1,5 +1,7 @@
 package org.group_three.ui.world;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
@@ -87,6 +89,11 @@ public class WorldObject {
 	 * @author Joel
 	 */
 	private final Color boxCollisionColor = UI.boxCollisionColor;
+
+
+	// ????
+	private final String detailClassPath = "";
+	private Object detailClass;
 
 
 	//++++++++++++++++++++++++++++++++++++++++++++++++++Constructor++++++++++++++++++++++++++++++++++++++++++++++++++
