b9729d1e7b0756c6f1f96f86ac602de77e3a03fa
Joel <joel.stark@stud.fra-uas.de>
2025-12-09 19:39:10 +0100

    Main class cleanup

--- src/main/java/org/group_three/Main.java
+++ src/main/java/org/group_three/Main.java
@@ -1,34 +1,18 @@
 package org.group_three;
 
-import de.tudresden.sumo.cmd.Route;
-import de.tudresden.sumo.objects.SumoStringList;
 import org.group_three.debug.Debug;
 import org.group_three.api.SimController;
-import org.group_three.debug.StaticTester;
-import org.group_three.model.WVehicle;
 import org.group_three.ui.MainApp;
-import de.tudresden.sumo.cmd.Trafficlight;
-import de.tudresden.sumo.cmd.Vehicle;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
-    public static void main(String[] args) {
-        Debug.print("Programm Start");
+	static void main(String[] args) {
+		Debug.print("Program Start");
 
-        //Tester, so this isn't full of random test calls
-        //Currently using speedtest.cfg
-        //new StaticTester();
-
-	    //SimController simController = new SimController("uascity/osm.sumocfg");
-	    //simController.setMainsim(true);
-
-        MainApp aMainGui = new MainApp();
-        aMainGui.start(args);
+		// Create and start the GUI
+		MainApp aMainGui = new MainApp();
+		aMainGui.start(args);
 
 		// Close main simulation on program end to make sure all threads get killed
-	    SimController.getMainstc().close();
-    }
+		SimController.getMainstc().close();
+	}
 }
