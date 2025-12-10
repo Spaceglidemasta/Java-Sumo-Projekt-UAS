fa84942f0639cab6e85ea1dc539b039ffa807bd9
Joel <joel.stark@stud.fra-uas.de>
2025-12-10 19:37:30 +0100

    implemented drawImage method in WorldVehicle class

--- src/main/java/org/group_three/ui/world/WorldVehicle.java
+++ src/main/java/org/group_three/ui/world/WorldVehicle.java
@@ -151,27 +151,9 @@ public class WorldVehicle extends WorldObject {
 	public void update() {
 		super.update();
 		Image visualImage = iconManager.getIcon(getColor());
-		Vector2D rect = new Vector2D(visualImage.getWidth(), visualImage.getHeight());
-		rect = rect.div(80 / scale_size);
-		getGraphicsContext().save();
-		//graphicsContext.setFill(Color.BLUE);
-		Vector2D drawLoc = Meth.addRelativeLocation(getWorld().getViewerPosition(),
-                getWorld().getViewerRotation(),
-                getPosition().mul(getWorld().getViewerZoom())
-        );
+		Vector2D imageSize = new Vector2D(visualImage.getWidth(), visualImage.getHeight()).div(80 / scale_size);
 
-		getGraphicsContext().translate(drawLoc.x + getWorld().getViewerPositionOffset().x,
-                                    drawLoc.y + getWorld().getViewerPositionOffset().y
-        ); // Object Location
-		getGraphicsContext().rotate(Meth.addRelativeRotation(getWorld().getViewerRotation(), getRotation()));
-		getGraphicsContext().drawImage(visualImage,
-                (rect.x / 2) * getWorld().getViewerZoom() * -1,
-                (rect.y / 2) * getWorld().getViewerZoom() * -1,
-                rect.x * getWorld().getViewerZoom(),
-                rect.y * getWorld().getViewerZoom()
-        );
-		//graphicsContext.fillRect((rect.x/2) * world.getViewerZoom() * -1, (rect.y/2) * world.getViewerZoom() * -1, rect.x * world.getViewerZoom(), rect.y * world.getViewerZoom());
-		getGraphicsContext().restore();
+		drawImage(imageSize.div(2), visualImage);
 	}
 
 	VehicleDetailsController vehicleDetailsController;
