cfe2483ad80e8e81ff0b9ab4ecffe33f7a3554ec
Joel <joel.stark@stud.fra-uas.de>
2025-12-13 16:44:17 +0100

    completed tail controller javadoc

--- src/main/java/org/group_three/ui/controllers/TailController.java
+++ src/main/java/org/group_three/ui/controllers/TailController.java
@@ -1,26 +1,22 @@
 package org.group_three.ui.controllers;
 
-import java.io.IOException;
-
-import org.group_three.debug.Debug;
-
 import javafx.fxml.FXML;
 
 /**
  * The small bar at the bottom of the window.
+ * No use yet.
  *
  * @author Joel
  */
 public class TailController {
+
 	/**
 	 * The default initialize method.
 	 *
-	 * @throws IOException Throw-Comment
 	 * @author Joel
 	 */
 	@FXML
-	public void initialize() throws IOException {
-		Debug.toConsole("Tail loaded.");
+	public void initialize() {
 	}
 
 }
