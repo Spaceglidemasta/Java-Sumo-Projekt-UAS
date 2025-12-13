19575035b4b521cc19412e6897a6d72a209d2ba2
Joel <joel.stark@stud.fra-uas.de>
2025-12-13 16:19:04 +0100

    some traffic light class clean up

--- src/main/java/org/group_three/ui/world/WorldTrafficLight.java
+++ src/main/java/org/group_three/ui/world/WorldTrafficLight.java
@@ -1,25 +1,12 @@
 package org.group_three.ui.world;
 
-import de.tudresden.sumo.objects.SumoPosition2D;
-import de.tudresden.sumo.objects.SumoStringList;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import org.group_three.api.SimController;
-import org.group_three.constants.UI;
-import org.group_three.debug.Debug;
 import org.group_three.model.WTrafficLight;
-import org.group_three.model.WVehicle;
-import org.group_three.ui.ColoredIconManager;
-import org.group_three.ui.Meth;
-import org.group_three.ui.Vector2D;
-import org.group_three.utils.Sumo2DLine;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
- * @author Leon
+ * The class to render TrafficLights.
+ *
+ * @author Joel
  */
 public class WorldTrafficLight extends WorldObject {
 //++++++++++++++++++++++++++++++++++++++++++++++++++MemberVariables++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -42,7 +29,6 @@ public class WorldTrafficLight extends WorldObject {
     public WorldTrafficLight(World world, Canvas canvas, String displayName, WTrafficLight trafficLight, int index) {
         super(world, canvas, displayName);
         this.wtl = trafficLight;
-
     }
     //--------------------------------------------------Constructors--------------------------------------------------
 
@@ -64,7 +50,7 @@ public class WorldTrafficLight extends WorldObject {
     //++++++++++++++++++++++++++++++++++++++++++++++++++Methods++++++++++++++++++++++++++++++++++++++++++++++++++
 
     /**
-     * The update method which is used to draw the WorldPoint in the world.
+     * The update method which is used to draw the WorldTrafficLight in the world.
      *
      * @author Joel
      */
