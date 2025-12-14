b6302b039a1ecb10f36611515a27d5f8071a37d0
Joel <joel.stark@stud.fra-uas.de>
2025-12-14 01:10:37 +0100

    + simulation speed modifier cant be <= 0, vehicle details show only last edge from route

--- src/main/java/org/group_three/ui/controllers/SimControlController.java
+++ src/main/java/org/group_three/ui/controllers/SimControlController.java
@@ -1,15 +1,21 @@
 package org.group_three.ui.controllers;
 
+import de.tudresden.sumo.objects.SumoStringList;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 import org.group_three.api.SimController;
 import org.group_three.debug.Debug;
+import org.group_three.model.WEdge;
+import org.group_three.ui.Meth;
 import org.group_three.ui.SimView2D;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SimControlController {
 
@@ -39,6 +45,7 @@ public class SimControlController {
 				(obs, oldText, newText) -> {
 					try {
 						speedModValue = Math.abs(Double.parseDouble(newText));
+						if (speedModValue <= 0) speedModValue = 0.1;
 						speedModifier.textProperty().set(String.valueOf(speedModValue));
 						timeline.setRate(speedModValue);
 
@@ -100,17 +107,17 @@ public class SimControlController {
 
 	private void onTick() {
 		onStepClicked();
-		/*
-		List<WEdge> roads = WEdge.getAllroads();
+
+		/*List<WEdge> roads = WEdge.getAllroads();
 		int randomIndex = ThreadLocalRandom.current().nextInt(roads.size());
 
 		SumoStringList strings = new SumoStringList();
-		strings.add(roads.get(randomIndex).getLaneIDs().getFirst());
+		strings.add(roads.get(randomIndex).getEdgeID());
 
 		SimController.getMainsimcon().addVehicle(
 				"DEFAULT_VEHTYPE",
 				SimController.getMainsimcon().addRoute(strings),
-				(int) time+1,
+				SimController.getMainsimcon().gettim,
 				0,
 				10
 				,0
