5b226c1ee6eef8ec516068c6353ac0e09114c9ad
masta of the universe <lucastarwars1@gmail.com>
2025-12-14 20:50:51 +0100

    Small changes to StatCollector

--- src/main/java/org/group_three/service/StatCollector.java
+++ src/main/java/org/group_three/service/StatCollector.java
@@ -1,9 +1,33 @@
 package org.group_three.service;
 
 
+import java.util.List;
+
 /**
  * The Stat stands for StatUtils, or for Static. Your choice.
  * @author Luca
  * */
 public class StatCollector {
+
+
+    private String name;
+    private List<Statistic> statistics;
+
+    public StatCollector(String name, Statistic ... args) {
+        this.name = name;
+        this.statistics = List.of(args);
+    }
+
+
+    @Deprecated
+    public void exportToGZ(){
+        for(Statistic stat : statistics){
+            stat.getTable().outAsCSV();
+        }
+
+
+    }
+
+
+
 }
