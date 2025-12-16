6aa2521247402db3356eba13421dabc3c16c309a
Joel <joel.stark@stud.fra-uas.de>
2025-12-16 19:18:03 +0100

    + save window for file exports, only extension selection actualy functional

--- src/main/java/org/group_three/ui/Meth.java
+++ src/main/java/org/group_three/ui/Meth.java
@@ -13,7 +13,7 @@ import java.util.List;
  *
  * @author Joel, Luca
  */
-public class Meth {
+public final class Meth {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++Constructors+++++++++++++++++++++++++++++++++++++++++++++++++++
 
