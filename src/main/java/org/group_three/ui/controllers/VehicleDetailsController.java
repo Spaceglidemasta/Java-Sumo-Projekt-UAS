42ffbc54bf3487f0e639014f683fa84c5c9608e4
Joel <joel.stark@stud.fra-uas.de>
2025-12-22 15:38:45 +0100

    + vehicle speed factor

--- src/main/java/org/group_three/ui/controllers/VehicleDetailsController.java
+++ src/main/java/org/group_three/ui/controllers/VehicleDetailsController.java
@@ -55,6 +55,15 @@ public class VehicleDetailsController {
 	private TextField speed;
 
 	/**
+	 * The vehicle speed to display.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
+	@FXML
+	private TextField speedFactor;
+
+	/**
 	 * The vehicle color to display.
 	 *
 	 * @author Joel
@@ -97,6 +106,9 @@ public class VehicleDetailsController {
 	@SuppressWarnings("JavadocDeclaration")
 	private WorldVehicle worldVehicle;
 
+	@FXML
+	private Button routeSelectButton;
+
 
 	/**
 	 * The initialize method of the vehicle details panel.
@@ -106,13 +118,13 @@ public class VehicleDetailsController {
 	@FXML
 	private void initialize() {
 
-		speed.textProperty().addListener((_, oldText, newText) -> {
+		speedFactor.textProperty().addListener((_, oldText, newText) -> {
 			try {
 				// set vehicle speed to a corrected value (for example "EEE" is not a valid speed) from the input
-				worldVehicle.getwVehicle().setSpeed(Math.abs(Double.parseDouble(newText)));
-				speed.setText(String.valueOf(Math.abs(Double.parseDouble(newText))));
+				worldVehicle.getwVehicle().setSpeedFactor(Math.abs(Double.parseDouble(newText)));
+				speedFactor.setText(String.valueOf(Math.abs(Double.parseDouble(newText))));
 			} catch (Exception e) {
-				speed.textProperty().set(oldText);
+				speedFactor.textProperty().set(oldText);
 			}
 		});
 
@@ -147,6 +159,11 @@ public class VehicleDetailsController {
 		SimView2D.getWorld().requestUpdate();
 	}
 
+	@FXML
+	private void onRouteSelectPressed() {
+
+	}
+
 	/**
 	 * The setup method for this class to fill it with data.
 	 *
@@ -160,6 +177,7 @@ public class VehicleDetailsController {
 		displayName.setText(worldVehicle.getDisplayName());
 		sumoId.setText(worldVehicle.getwVehicle().getID());
 		speed.setText(String.valueOf(worldVehicle.getwVehicle().getSpeed()));
+		speedFactor.setText(String.valueOf(worldVehicle.getwVehicle().getSpeedFactor()));
 		color.setValue(worldVehicle.getColor());
 	}
 
@@ -170,8 +188,8 @@ public class VehicleDetailsController {
 	 */
 	public void update() {
 		speed.setText(String.valueOf(worldVehicle.getwVehicle().getSpeed()));
+		speedFactor.setText(String.valueOf(worldVehicle.getwVehicle().getSpeedFactor()));
 		color.setValue(worldVehicle.getColor());
-		route.setText(worldVehicle.getwVehicle().getRouteEdges().getLast());
 	}
 
 	/**
@@ -187,7 +205,7 @@ public class VehicleDetailsController {
 		sumoId.setDisable(true);
 		speed.setDisable(true);
 		color.setDisable(true);
-		route.setDisable(true);
+		routeSelectButton.setDisable(true);
 		viewLockButton.setDisable(true);
 	}
 }
