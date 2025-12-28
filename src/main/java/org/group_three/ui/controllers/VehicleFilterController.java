48fb18f368cf6609311240ec19e77156d1e9bae2
Joel <joel.stark@stud.fra-uas.de>
2025-12-28 23:39:45 +0100

    + functional speed filter

--- src/main/java/org/group_three/ui/controllers/VehicleFilterController.java
+++ src/main/java/org/group_three/ui/controllers/VehicleFilterController.java
@@ -1,4 +1,38 @@
 package org.group_three.ui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.group_three.constants.UI;
+import org.group_three.ui.Vector2D;
+
+
 public class VehicleFilterController {
+
+	@FXML private TextField speedMin;
+
+	@FXML private TextField speedMax;
+
+
+	@FXML
+	private void initialize() {
+		speedMin.textProperty().addListener((_, oldText, newText) -> {
+			try {
+				// set vehicle speed to a corrected value (for example "EEE" is not a valid speed) from the input
+				UI.viewFilter_VehicleSpeed = new Vector2D(Math.abs(Double.parseDouble(newText)), UI.viewFilter_VehicleSpeed.y);
+				speedMin.setText(String.valueOf(Math.abs(Double.parseDouble(newText))));
+			} catch (Exception e) {
+				speedMin.textProperty().set(oldText);
+			}
+		});
+
+		speedMax.textProperty().addListener((_, oldText, newText) -> {
+			try {
+				// set vehicle speed to a corrected value (for example "EEE" is not a valid speed) from the input
+				UI.viewFilter_VehicleSpeed = new Vector2D(UI.viewFilter_VehicleSpeed.x, Math.abs(Double.parseDouble(newText)));
+				speedMax.setText(String.valueOf(Math.abs(Double.parseDouble(newText))));
+			} catch (Exception e) {
+				speedMax.textProperty().set(oldText);
+			}
+		});
+	}
 }
