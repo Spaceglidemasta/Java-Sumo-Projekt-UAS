f7f876dad9fa7a4d233a753422d9c448d051dfc4
Joel <joel.stark@stud.fra-uas.de>
2026-01-05 22:17:19 +0100

    Main class javadoc

--- src/main/java/org/group_three/Main.java
+++ src/main/java/org/group_three/Main.java
@@ -4,7 +4,21 @@ import org.group_three.debug.Debug;
 import org.group_three.api.SimController;
 import org.group_three.ui.MainApp;
 
+/**
+ * The main class which creates the MainApp and does sumo process clean up.
+ *
+ * @author Joel, Luca
+ */
 public class Main {
+
+	/**
+	 * The main method for this whole project.
+	 * This is where the MainApp get created and
+	 * when the app closes it closes the sumo simulation if needed.
+	 *
+	 * @param args The launch/start arguments array.
+	 * @author Joel, Luca
+	 */
 	static void main(String[] args) {
 		Debug.print("Program Start");
 
@@ -13,6 +27,8 @@ public class Main {
 		aMainGui.start(args);
 
 		// Close main simulation on program end to make sure all threads get killed
-        if(SimController.getMainsimcon() != null) SimController.getMainsimcon().close();
+		SimController simcon = SimController.getMainsimcon();
+		if (simcon != null) simcon.close();
 	}
+
 }
