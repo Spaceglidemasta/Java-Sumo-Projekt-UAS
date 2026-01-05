ff2916d4a7707be077eb9b3161cfd06c903a189e
Joel <joel.stark@stud.fra-uas.de>
2026-01-05 21:31:42 +0100

    stat data ui controller javadoc

--- src/main/java/org/group_three/ui/controllers/PDFCreationFilterController.java
+++ src/main/java/org/group_three/ui/controllers/PDFCreationFilterController.java
@@ -13,65 +13,132 @@ import org.group_three.constants.enums.stats.EdgeSortOption;
 import org.group_three.constants.enums.style.CSSStyle;
 import org.group_three.ui.Meth;
 
+/**
+ * The controller for the pdf/csv stat data export.
+ *
+ * @author Joel
+ */
 public class PDFCreationFilterController {
 
+	/**
+	 * The stage which this FXML/Controller is part of.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	public Stage stage;
 
+	/**
+	 * The street length filter.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	@FXML
 	private TextField length;
 
+	/**
+	 * The vehicle color filter.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	@FXML
 	private ColorPicker color;
 
+	/**
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	@FXML
 	private CheckBox avgSpeed;
 
+	/**
+	 * The ComboBox for the edge sort.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	@FXML
 	private ComboBox<EdgeSortOption> edgeSort;
 
+	/**
+	 * The ComboBox for the pdf's CSS style.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
 	@FXML
 	private ComboBox<CSSStyle> style;
 
+	/**
+	 * The initialize method of this controller.
+	 * Initializes the ComboBoxes.
+	 *
+	 * @author Joel
+	 */
 	@FXML
 	private void initialize() {
+		// Ini edge sort combo box
 		edgeSort.getItems().addAll(EdgeSortOption.values());
 		edgeSort.getSelectionModel().select(EdgeSortOption.usage);
 
+		// Ini pdf style combo box
 		style.getItems().addAll(CSSStyle.values());
 		style.getSelectionModel().select(CSSStyle.DEFAULT);
 	}
 
+	/**
+	 * A method that gets called when the save button is pressed.
+	 * Grabs the data based on the given parameters from the FXML and exports it.
+	 *
+	 * @author Joel
+	 */
 	@FXML
 	private void onSave() {
 		SimController simcon = SimController.getMainsimcon();
 
-		if(simcon == null) {
+		if (simcon == null) {
+			// close window
 			stage.close();
 			return;
 		}
 
+		// collect stat data
 		simcon.queueryStats(
 
-                DefaultStasticValues.STATCOLLECTION_NAME,
+				// data name
+				DefaultStasticValues.STATCOLLECTION_NAME,
 
+				// veh data name
 				DefaultStasticValues.VEHSTAT_NAME,
+				// sort by avg vehicle speed bool
 				avgSpeed.isSelected(),
+				// filter for specific vehicle color, if opacity 0 no filter
 				(color.getValue().getOpacity() != 0) ? Meth.ClrToSumoClr(color.getValue()) : null,
 
-                DefaultStasticValues.EDGESTAT_NAME,
+				// edge data name
+				DefaultStasticValues.EDGESTAT_NAME,
+				// sort data by street name, usage or street length
 				edgeSort.getValue(),
+				// filter streets below this length, "0" added in front so integer parsing still works if no input is present
 				Integer.parseInt("0" + length.getText()),
 
-                style.getValue()
+				// pdf style
+				style.getValue()
 
 		);
 
+		// export stat data
 		if (stage.getTitle().contains(Documents.CSV_EXTENSION)) {
+			// export as csv
 			simcon.exportStatsAsZippedCSVs();
 		} else {
+			// export as pdf
 			simcon.exportStatsToPDF();
 		}
 
+		// close window
 		stage.close();
 	}
 }
