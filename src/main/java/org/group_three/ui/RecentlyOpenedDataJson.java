9a9bff9c039779e42756958487eee4e972f110c1
Joel <joel.stark@stud.fra-uas.de>
2026-01-05 21:39:25 +0100

    some more javadoc

--- src/main/java/org/group_three/ui/RecentlyOpenedDataJson.java
+++ src/main/java/org/group_three/ui/RecentlyOpenedDataJson.java
@@ -2,12 +2,35 @@ package org.group_three.ui;
 
 import java.util.List;
 
+/**
+ * A JSON data class.
+ *
+ * @author Joel
+ */
 public class RecentlyOpenedDataJson {
 
-	public RecentlyOpenedDataJson () {}
-	public RecentlyOpenedDataJson (List<String> sims) {
-		this.sims = sims;
+	/**
+	 * The list of recently loaded simulations.
+	 *
+	 * @author Joel
+	 */
+	@SuppressWarnings("JavadocDeclaration")
+	public List<String> sims;
+
+	/**
+	 * The empty constructor to potentially load data in later.
+	 *
+	 * @author Joel
+	 */
+	public RecentlyOpenedDataJson() {
 	}
 
-	public List<String> sims;
+	/**
+	 * The normal constructor to load this JSON data class with data.
+	 *
+	 * @param sims The list of recently loaded simulations.
+	 */
+	public RecentlyOpenedDataJson(List<String> sims) {
+		this.sims = sims;
+	}
 }
